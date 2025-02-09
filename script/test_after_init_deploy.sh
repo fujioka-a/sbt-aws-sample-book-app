5e735a2329a3f1bcdc1a01b470d533c97490be8c
fujioka <atsushi3233@gmail.com>
2025-02-09 09:12:50 +0900

    初期デプロイ後のテストスクリプトを追加

---
+++ script/test_after_init_deploy.sh
@@ -0,0 +1,90 @@
+# 第一引数から PASSWORD を取得
+PASSWORD=$1
+PROFILE=$2
+
+if [ -z "$PASSWORD" ]; then
+  echo "Error: PASSWORD is not provided."
+  echo "Usage: $0 <PASSWORD>"
+  exit 1
+fi
+
+if [ -z "$PROFILE" ]; then
+  echo "Error: PROFILE is not provided."
+  echo "Usage: $1 <PROFILE>"
+  exit 1
+fi
+
+
+# Change this to a real email if you'd like to log into the tenant
+TENANT_EMAIL="tenant@example.com"
+CONTROL_PLANE_STACK_NAME="ControlPlaneStack"
+TENANT_NAME="tenant$RANDOM"
+
+CLIENT_ID=$(aws cloudformation describe-stacks \
+  --stack-name "$CONTROL_PLANE_STACK_NAME" \
+  --query "Stacks[0].Outputs[?OutputKey=='ControlPlaneIdpClientId'].OutputValue" \
+  --output text \
+  --profile "$PROFILE")
+
+USER_POOL_ID=$(aws cloudformation describe-stacks \
+  --stack-name "$CONTROL_PLANE_STACK_NAME" \
+  --query "Stacks[0].Outputs[?OutputKey=='ControlPlaneIdpUserPoolId'].OutputValue" \
+  --output text \
+  --profile "$PROFILE")
+
+USER="admin"
+
+# required in order to initiate-auth
+aws cognito-idp update-user-pool-client \
+    --user-pool-id "$USER_POOL_ID" \
+    --client-id "$CLIENT_ID" \
+    --explicit-auth-flows USER_PASSWORD_AUTH \
+    --profile "$PROFILE"
+
+# remove need for password reset
+aws cognito-idp admin-set-user-password \
+    --user-pool-id "$USER_POOL_ID" \
+    --username "$USER" \
+    --password "$PASSWORD" \
+    --permanent \
+    --profile "$PROFILE"
+
+# get credentials for user
+AUTHENTICATION_RESULT=$(aws cognito-idp initiate-auth \
+  --auth-flow USER_PASSWORD_AUTH \
+  --client-id "${CLIENT_ID}" \
+  --auth-parameters "USERNAME='${USER}',PASSWORD='${PASSWORD}'" \
+  --query 'AuthenticationResult' \
+  --profile "$PROFILE")
+
+ACCESS_TOKEN=$(echo "$AUTHENTICATION_RESULT" | jq -r '.AccessToken')
+
+CONTROL_PLANE_API_ENDPOINT=$(aws cloudformation describe-stacks \
+    --stack-name "$CONTROL_PLANE_STACK_NAME" \
+    --query "Stacks[0].Outputs[?contains(OutputKey,'controlPlaneAPIEndpoint')].OutputValue" \
+    --output text \
+    --profile "$PROFILE")
+
+DATA=$(jq --null-input \
+    --arg tenantName "$TENANT_NAME" \
+    --arg tenantEmail "$TENANT_EMAIL" \
+    '{
+  "tenantName": $tenantName,
+  "email": $tenantEmail,
+  "tier": "basic",
+  "tenantStatus": "In progress"
+}')
+
+echo "creating tenant..."
+curl --request POST \
+    --url "${CONTROL_PLANE_API_ENDPOINT}tenants" \
+    --header "Authorization: Bearer ${ACCESS_TOKEN}" \
+    --header 'content-type: application/json' \
+    --data "$DATA" | jq
+echo "" # add newline
+
+echo "retrieving tenants..."
+curl --request GET \
+    --url "${CONTROL_PLANE_API_ENDPOINT}tenants" \
+    --header "Authorization: Bearer ${ACCESS_TOKEN}" \
+    --silent | jq
